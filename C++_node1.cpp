87af048c3d7684a4100c131ef173a70c3a9eaee1
CLinchao <948564096@qq.com>
2021-01-20 14:47:45 +0800

    WTU _left

---
+++ C++_node1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<cstdio>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+
+
--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    Sample2 s1(2),s2(s1),s3;
-    s3=s2;
-    s1.disp();
-   // s1.disp1();
-    s2.disp();
-    s3.disp();
